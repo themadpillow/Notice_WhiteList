8b1ec4282b8dbb916d50013509e4614a605b76a0
themadpillow <mouhanpatumakura@gmail.com>
2018-11-12 19:13:30 +0900

    Java既存の機能で書き換え

--- src/nwl/GetLatestMCID.java
+++ src/nwl/GetLatestMCID.java
@@ -1,40 +1,67 @@
 package nwl;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.UUID;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class GetLatestMCID {
 	private String url = "https://sessionserver.mojang.com/session/minecraft/profile/";
-	OkHttpClient client;
-	Request request;
+	HttpURLConnection http;
 	UUID uuid;
 
 	public GetLatestMCID(UUID uuid) {
 		this.uuid = uuid;
 		url += uuid.toString().replaceAll("-", "");
-		client = new OkHttpClient();
-		request = new Request.Builder().url(url).build();
+		try {
+			http = (HttpURLConnection) new URL(url).openConnection();
+			http.setRequestMethod("GET");
+			http.connect();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 	}
 
 	public String get() {
-		String name = null;
+		String response = null;
 		try {
-			Response response = client.newCall(request).execute();
-			JSONObject jsonObject = new JSONObject(response.body().string());
-			name = jsonObject.getString("name");
+			if ((response = getResponse(http)) == null) {
+				return null;
+			}
 		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (JSONException e) {
-			e.printStackTrace();
+			return null;
 		}
+		String name = null;
+		JsonParser jsonParser = new JsonParser();
+		JsonObject jsonObject = jsonParser.parse(response).getAsJsonObject();
+		name = jsonObject.get("name").getAsString();
 
 		return name;
+	}
+
+	private String getResponse(HttpURLConnection urlConnection) throws IOException {
+		int statusCode = urlConnection.getResponseCode();
+		if (statusCode != 200) {
+			urlConnection.disconnect();
+			return null;
+		}
+
+		InputStream stream = urlConnection.getInputStream();
+		StringBuffer sb = new StringBuffer();
+		String line = "";
+		BufferedReader br = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+		while ((line = br.readLine()) != null) {
+			sb.append(line);
+		}
+		stream.close();
+		urlConnection.disconnect();
+		return sb.toString();
 	}
 }
